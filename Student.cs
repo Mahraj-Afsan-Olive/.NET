ef7c0aa7b9bdb539e373dde060095764d6b1ef9c
Olive <104560789+Mahraj-Afsan-Olive@users.noreply.github.com>
2025-03-17 12:01:28 +0600

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FormSubmission.Models
+{
+    public class Student
+    {
+        [Required]
+        [RegularExpression(@"^\d{2}-\d{5}-[1-3]$", ErrorMessage = "Please provide correct Id")]
+        public string Id { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z._\s]*$", ErrorMessage ="Please provide right format for name.")]
+        public string Name { get; set; }
+        [Required]
+        [IdMatchesEmail(ErrorMessage = "Email must match the Id format (e.g., Id@student.aiub.edu).")]
+        public string Email { get; set; }
+        [Required]
+        public string Dob { get; set; }
+
+    }
+    
+}
